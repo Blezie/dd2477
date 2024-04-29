d1f9d671a3016f78e58955d2dd21d08ac0e02e60
senneve <66268408+senneve@users.noreply.github.com>
2024-04-29 16:47:36 +0200

    Fixed index bug and changed code so graphs for all methods are together on one plot

--- evaluation/plot_pr.py
+++ evaluation/plot_pr.py
@@ -34,13 +34,15 @@ for i in range(1,NUM_OF_QUERIES+1):
         newQueryFilenames.append(methodName+"/"+methodName+f"_query{i}_results.txt")
     queries.append(newQueryFilenames)
 
+"""
+# For individual curves
 for i, query_filenames in enumerate(queries, start=1):
     for j in range(len(query_filenames)): # All methods for query_i
         filename = query_filenames[j]
         methodName = filename.split("/")[0] # Extract the method name from the filename
         results = readFile(path + filename)
-        PRE = np.array([precision(results[:i]) for i in range(1, len(results), 3)]) # 16 results
-        REC = np.array([recall(results[:i]) for i in range(1, len(results), 3)])
+        PRE = np.array([precision(results[:i]) for i in range(1, len(results)+1, 3)]) # 15 results
+        REC = np.array([recall(results[:i]) for i in range(1, len(results)+1, 3)])
         
         print(f"Query {i}, {filename}: Precision {np.round(PRE * 100) / 100}, Recall {REC}")
 
@@ -55,3 +57,29 @@ for i, query_filenames in enumerate(queries, start=1):
         plt.savefig(save_path) # Save plot to file
 
         plt.show()
+"""
+
+# All curves for 1 query in 1 plot
+colors = plt.cm.get_cmap('tab10', len(methods))
+for i, query_filenames in enumerate(queries, start=1):
+    for j in range(len(query_filenames)): # All methods for query_i
+        filename = query_filenames[j]
+        methodName = filename.split("/")[0] # Extract the method name from the filename
+        results = readFile(path + filename)
+        PRE = np.array([precision(results[:i]) for i in range(1, len(results)+1, 3)]) # 15 results
+        REC = np.array([recall(results[:i]) for i in range(1, len(results)+1, 3)])
+        
+        print(f"Query {i}, {filename}: Precision {np.round(PRE * 100) / 100}, Recall {REC}")
+
+        plt.plot(REC, PRE, color=colors(j), label=methodName) # Plot with color and label
+        
+    plt.axis([np.min(REC), np.max(REC), 0, 1.1])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title(f"Precision-Recall curve for Query {i}")
+    plt.legend() # Add a legend to the plot
+
+    save_path = os.path.join(path, f"PR_curve_query{i}.png")
+    plt.savefig(save_path) # Save plot to file
+
+    plt.show()
